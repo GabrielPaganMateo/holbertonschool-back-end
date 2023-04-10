d46b480b579666d4a0b0ce4d365b7e139add5434
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-04-10 16:53:43 -0400

    DONE

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -7,28 +7,27 @@ employee ID to return info about his/her todo list progress
 if __name__ == '__main__':
     import json
     from requests import get
-    from sys import argv
     """this module is fucking documented"""
 
     all_todos = get(f'https://jsonplaceholder.typicode.com/todos')
     users = get(f'https://jsonplaceholder.typicode.com/users')
 
-    employee_list = json.loads(all_todos.text)
+    todo_list = json.loads(all_todos.text)
     users = json.loads(users.text)
 
     task_count = 0
     task_list = []
     task_success = []
-    for employee in employee_list:
-        task_list.append(employee['title'])
-        task_success.append(employee['completed'])
-
     json_list = []
     THE_dict = {}
     user_count = 0
     for user in users:
         user_count += 1
         employee_name = user.get('username')
+        for employee in todo_list:
+            if employee.get('userId') == user.get('id'):
+                task_list.append(employee['title'])
+                task_success.append(employee['completed'])
         for i in range(len(task_list)):
             true_or_false = bool(task_success[i])
             task = f'{task_list[i]}'
@@ -37,8 +36,10 @@ if __name__ == '__main__':
                            'task': task,
                            'completed': true_or_false})
             json_list.append(every_dict)
+        
         THE_dict[str(user_count)] = json_list
-
-    print(THE_dict)
+        json_list = []
+        task_list = []
+        task_success = []
     with open('todo_all_employees.json', 'w') as f:
         json.dump(THE_dict, f)

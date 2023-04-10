f33c8e92be1140ebd95f8cd6a33b3f3dde1bc4fe
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-04-10 11:07:55 -0400

    Add task 0

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Oythin script that uses this 
+'https://jsonplaceholder.typicode.com/todos/1'
+rest api for given employee ID to return
+info about his/her todo list progress"""
+from sys import argv
+from requests import get
+import json
+
+employee_ID = argv[1]
+employee_info = get(f'https://jsonplaceholder.typicode.com/todos/{employee_ID}')
+all_todos = get(f'https://jsonplaceholder.typicode.com/todos')
+
+employee_dict = json.loads(employee_info.text)
+employee_list = json.loads(all_todos.text)
+
+print(employee_dict)
+employee_name = employee_dict['userId']
+completed_task = 0
+task_count = 0
+task_list = []
+for employee in employee_list:
+    if employee['userId'] == employee_dict['userId']:
+        task_count += 1
+        if employee['completed'] == True:
+            completed_task += 1
+            task_list.append(employee['title'])
+        
+
+
+print(f'Employee {employee_name} is done with tasks{completed_task}/{task_count}:')
+
+for task in task_list:
+    print(f'     {task}')
